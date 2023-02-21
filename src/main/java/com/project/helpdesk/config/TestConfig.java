335a4c5843c9d3c0cda763925f2942ae99d5eb9f
jaum-fs <martins_joao@id.uff.br>
2023-02-20 21:34:13 -0300

    test profile created

---
+++ src/main/java/com/project/helpdesk/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.project.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.project.helpdesk.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbS;
+
+    @Bean
+    void instanciaDb() {
+        dbS.dbInstance();
+    }
+	
+}
---
+++ src/main/java/com/project/helpdesk/services/DBService.java
@@ -0,0 +1,47 @@
+package com.project.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.helpdesk.domain.Chamado;
+import com.project.helpdesk.domain.Cliente;
+import com.project.helpdesk.domain.Tecnico;
+import com.project.helpdesk.domain.enums.Perfil;
+import com.project.helpdesk.domain.enums.Prioridade;
+import com.project.helpdesk.domain.enums.Status;
+import com.project.helpdesk.repositories.ChamadoRepository;
+import com.project.helpdesk.repositories.ClienteRepository;
+import com.project.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	
+	public void dbInstance() {
+		
+		Tecnico t1 = new Tecnico(null, "Valdir Cezar", "6365320268", "vadir@mail.com", "123" );
+		t1.addPerfil(Perfil.ADMIN);
+		
+		Cliente c1 = new Cliente(null, "Linus Tovalds", "63614523658", "torvalds@mail.com", "123");
+		
+		Chamado call = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", t1, c1);
+		
+		
+		tecnicoRepository.saveAll(Arrays.asList(t1));
+		clienteRepository.saveAll(Arrays.asList(c1));
+		chamadoRepository.saveAll(Arrays.asList(call));
+		
+	}
+	
+}
